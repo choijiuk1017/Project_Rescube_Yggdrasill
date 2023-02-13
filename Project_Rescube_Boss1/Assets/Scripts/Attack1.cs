4197304edaee17241095e0ad4359b8e980ac0383
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2023-02-13 22:56:35 +0900

    공격 패턴 1 전조 이미지 추가

--- Project_Rescube_Boss1/Assets/Scripts/Attack1.cs
+++ Project_Rescube_Boss1/Assets/Scripts/Attack1.cs
@@ -7,12 +7,14 @@ public class Attack1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 2f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+
     }
 }
